1a1664101cb35c5482c28fa76a6d2ffde9edbe83
Jonas Lykke <jona711p@student.eadania.dk>
2017-11-01 08:25:09 +0100

    Works?

--- server.ts
+++ server.ts
@@ -46,10 +46,9 @@ class Server {
 
     // Cors
     this.app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
-      res.header('Access-Control-Allow-Credentials', 'true');
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
       next();
     });
   }
